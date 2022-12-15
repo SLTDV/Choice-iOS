56307f52736c5218d6576bcd1c6dd3c1e20dee42
Mindohyeon <min50875@naver.com>
2022-12-15 19:45:41 +0900

    :lipstick: Add Titlelabel

--- Sources/Present/Auth/SignIn/ViewController/SignInViewController.swift
+++ Sources/Present/Auth/SignIn/ViewController/SignInViewController.swift
@@ -9,5 +9,16 @@
 import UIKit
 
 class SignInViewController: BaseVC<SignInViewModel> {
+    private let titleLabel = UILabel().then {
+        $0.text = "셀렉트"
+        $0.textColor = .black
+        $0.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 28)
+    }
+    
+    private let subTitleLabel = UILabel().then {
+        $0.text = "선택의 고민을 한 번에"
+        $0.textColor = .black
+        $0.font = UIFont(name: "AppleSDGothicNeo-Light", size: 16)
+    }
 }
 
