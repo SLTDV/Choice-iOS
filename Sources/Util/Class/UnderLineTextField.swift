938e9dab59c924b8acdd5e6987d388d151d71488
Mindohyeon <min50875@naver.com>
2022-12-16 11:21:06 +0900

    :lipstick: Update UnderLineTextField - left padding, font size

--- Sources/Util/Class/UnderLineTextField.swift
+++ Sources/Util/Class/UnderLineTextField.swift
@@ -22,6 +22,9 @@ class UnderLineTextField: UITextField {
     }
     
     func setPlaceholder(placeholder: String) {
+        self.font = .systemFont(ofSize: 14)
+        self.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 5.0, height: 0.0))
+        self.leftViewMode = .always
         self.attributedPlaceholder = NSAttributedString(
             string: placeholder,
             attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray]
