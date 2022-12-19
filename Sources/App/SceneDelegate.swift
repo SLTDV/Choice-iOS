59cbb0e12eea26d92f7b1ce2b920e52af347cccb
Mindohyeon <min50875@naver.com>
2022-12-19 11:24:00 +0900

    :lipstick: Add titleLabel, descriptionLabel, imageView

--- Sources/App/SceneDelegate.swift
+++ Sources/App/SceneDelegate.swift
@@ -10,6 +10,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        UIFont.familyNames.sorted().forEach { familyName in
+            print("*** \(familyName) ***")
+            UIFont.fontNames(forFamilyName: familyName).forEach { fontName in
+                print("\(fontName)")
+            }
+            print("---------------------")
+        }
         
         let navVC = UINavigationController()
         
---
+++ Sources/Present/Main/Cell/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewCell.swift
+//  Choice
+//
+//  Created by 민도현 on 2022/12/19.
+//  Copyright © 2022 com.dohyeon. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class PostCell: UITableViewCell {
+    private let titleLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 16, weight: .semibold)
+    }
+    
+    private let descriptionLabel = UILabel().then {
+        $0.numberOfLines = 0
+        $0.font = .systemFont(ofSize: 12)
+    }
+    
+    private let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+    }
+}
