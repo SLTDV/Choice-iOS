5456486f833d6d57aebe38e66095bc65044bf47e
Mindohyeon <min50875@naver.com>
2022-12-26 18:09:51 +0900

    :sparkles: Add createPost

--- Sources/Present/AddPost/ViewModel/AddPostViewModel.swift
+++ Sources/Present/AddPost/ViewModel/AddPostViewModel.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Alamofire
-import RxSwift
 
 class AddPostViewModel: BaseViewModel {
     func createPost(title: String, content: String, imageData: UIImage, firstVotingOption: String, secondVotingOtion: String) {
@@ -8,45 +7,44 @@ class AddPostViewModel: BaseViewModel {
         
         let headers: HTTPHeaders = ["Content-Type" : "multipart/form-data"]
         let params = [
-            "key" : "req",
-            "value" : ["title" : title,
-                       "content" : content,
-                       "firstVotingOption" : firstVotingOption,
-                       "secondVotingOption" : secondVotingOtion]
-        ] as Dictionary
-        
-        AF.upload(multipartFormData: { multipartFormData in
-            for (key, value) in params {
-                multipartFormData.append("\(value)".data(using: .utf8)!, withName: key)
-            }
-            if let image = imageData.pngData() {
-                multipartFormData.append(image, withName: "file", fileName: "\(image).png", mimeType: "image/png")
-            }
-        }, to: url, method: .post, headers: headers).validate().responseData { response in
-            print(response.response?.statusCode)
-            
-            switch response.result {
-            case .success(let data):
-                print("success")
-            case .failure(let error):
-                print("error \(error.localizedDescription)")
-            }
-        }
+            "title" : title,
+            "content" : content,
+            "firstVotingOption" : firstVotingOption,
+            "secondVotingOption" : secondVotingOtion
+            ] as Dictionary
         
         AF.request(url,
                    method: .post,
                    parameters: params,
                    encoding: URLEncoding.queryString,
                    headers: headers)
-            .responseData { [weak self] response in
-                switch response.result {
-                case .success:
-                    print(response.response?.statusCode)
-                    self?.coordinator.navigate(to: .popAddpostIsRequired)
-
-                case .failure(let error):
-                    print("error = \(error.errorDescription)")
-                }
+        .responseData { [weak self] response in
+            switch response.result {
+            case .success:
+                print(response.response?.statusCode)
+                self?.coordinator.navigate(to: .popAddpostIsRequired)
+                
+            case .failure(let error):
+                print("error = \(error.errorDescription)")
             }
+        }
+        
+//        AF.upload(multipartFormData: { multipartFormData in
+//            for (key, value) in params {
+//                multipartFormData.append("\(value)".data(using: .utf8)!, withName: key)
+//            }
+//            if let image = imageData.pngData() {
+//                multipartFormData.append(image, withName: "file", fileName: "\(image).png", mimeType: "image/png")
+//            }
+//        }, to: url, method: .post, headers: headers).validate().responseData { response in
+//            print(response.response?.statusCode)
+//
+//            switch response.result {
+//            case .success(let data):
+//                print("success")
+//            case .failure(let error):
+//                print("error \(error.localizedDescription)")
+//            }
+//        }
     }
 }
