80c08ca76fa5e2a9d00735ff49ec86a5e98152cf
Mindohyeon <min50875@naver.com>
2022-12-27 11:23:03 +0900

    :sparkles: Add Image Upload 성공시 게시물 생성 요청

--- Sources/Present/AddPost/ViewModel/AddPostViewModel.swift
+++ Sources/Present/AddPost/ViewModel/AddPostViewModel.swift
@@ -25,38 +25,38 @@ class AddPostViewModel: BaseViewModel {
             case .success(let data):
                 let decodeResponse = try? JSONDecoder().decode(AddPostModel.self, from: data)
                 print(decodeResponse?.imageUrl)
-                print("status code = \(response.response?.statusCode)")
-                
-                let imagUrl = decodeResponse?.imageUrl ?? ""
-                url = APIConstants.createPostURL
-                params = [
-                    "title" : title,
-                    "content" : content,
-                    "firstVotingOption" : firstVotingOption,
-                    "secondVotingOption" : secondVotingOtion,
-                    "thumbnail" : imagUrl
-                ] as Dictionary
-                
-                print(params)
-                AF.request(url,
-                           method: .post,
-                           parameters: params,
-                           encoding: URLEncoding.queryString,
-                           headers: headers)
-                .validate()
-                .responseData { [weak self] response in
-                    switch response.result {
-                    case .success:
-                        print(response.response?.statusCode)
-                        self?.coordinator.navigate(to: .popAddpostIsRequired)
-                        
-                    case .failure(let error):
-                        print("error = \(error.errorDescription)")
-                    }
-                }
-                print("success = \(data)")
+                print("status code1 = \(response.response?.statusCode)")
             case .failure(let error):
-                print("error \(error.localizedDescription)")
+                print("error \(error.errorDescription)")
+            }
+            
+            headers = ["Content-Type": "application/json"]
+            let imagUrl = "https://choice-bucket.s3.ap-northeast-2.amazonaws.com/images/2583766%20bytes.png"
+            url = APIConstants.createPostURL
+            params = [
+                "title" : title,
+                "content" : content,
+                "firstVotingOption" : firstVotingOption,
+                "secondVotingOption" : secondVotingOtion,
+                "thumbnail" : imagUrl
+            ]
+            
+            print(params)
+            AF.request(url,
+                       method: .post,
+                       parameters: params,
+                       encoding: JSONEncoding.default,
+                       headers: headers)
+            .validate()
+            .responseData { [weak self] response in
+                switch response.result {
+                case .success:
+                    print(response.response?.statusCode)
+                    self?.coordinator.navigate(to: .popAddpostIsRequired)
+                    
+                case .failure(let error):
+                    print("error = \(String(describing: error.errorDescription))")
+                }
             }
         }
     }
