152cbcd04d18a4437e9902fcb6701f1071db49e8
Mindohyeon <min50875@naver.com>
2022-12-27 13:56:22 +0900

    :sparkles: Add signInURL

--- Sources/Present/Network/APIConstants.swift
+++ Sources/Present/Network/APIConstants.swift
@@ -3,6 +3,9 @@ import Foundation
 final class APIConstants {
     static let baseURL = "http://10.82.17.76:8090"
     
+    //SignIn
+    static let signInURL = baseURL + "/auth/signin"
+    
     static let findNewestPost = baseURL + "/post/"
     static let findAllBestPost = baseURL + "/post/list"
     static let signUpURL = baseURL + "/auth/signUp"
