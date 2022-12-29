01c4bdee2146dc2b974a88ba064f692336daf0d6
Heesang <ghs050611@naver.com>
2022-12-29 15:43:01 +0900

    :memo: 코드 정리

--- Sources/Present/Detail/ViewController/DetailPostViewController.swift
+++ Sources/Present/Detail/ViewController/DetailPostViewController.swift
@@ -48,12 +48,12 @@ final class DetailPostViewController: BaseVC<DetailPostViewModel> {
     }
     
     init(viewModel: DetailPostViewModel, model: PostModel) {
-            super.init(viewModel: viewModel)
-            self.model = model
+        super.init(viewModel: viewModel)
+        self.model = model
     }
     
     required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
+        fatalError("init(coder:) has not been implemented")
     }
     
     private func changePostData(model: PostModel) {
@@ -65,19 +65,13 @@ final class DetailPostViewController: BaseVC<DetailPostViewModel> {
             }
             self.voteView.changeVoteTitleData(with: model)
         }
-        
-//        DispatchQueue.main.async {
-//            voteView.changeVoteTitleData(with:)
-//        }
     }
     
-    //뷰가 나타나기 직전
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.commentTableView.addObserver(self, forKeyPath: "contentSize", options: .new, context: nil)
     }
     
-    //화면에 나타난 직후
     override func viewWillDisappear(_ animated: Bool) {
         self.commentTableView.removeObserver(self, forKeyPath: "contentSize")
     }
@@ -129,25 +123,25 @@ final class DetailPostViewController: BaseVC<DetailPostViewModel> {
             $0.top.equalTo(titleLabel.snp.bottom).offset(10)
             $0.leading.equalToSuperview().offset(20)
         }
-
+        
         postImageView.snp.makeConstraints {
             $0.top.equalTo(descriptionLabel.snp.bottom).offset(10)
             $0.centerX.equalToSuperview()
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(200)
         }
-
+        
         voteView.snp.makeConstraints {
             $0.top.equalTo(postImageView.snp.bottom).offset(31)
             $0.leading.trailing.equalToSuperview().inset(10)
         }
-
+        
         divideLineView.snp.makeConstraints {
             $0.top.equalTo(voteView.snp.bottom).offset(50)
             $0.leading.trailing.equalToSuperview().inset(30)
             $0.height.equalTo(1)
         }
-
+        
         commentCountLabel.snp.makeConstraints {
             $0.top.equalTo(divideLineView.snp.bottom).offset(18)
             $0.leading.equalToSuperview().offset(30)
@@ -158,7 +152,7 @@ final class DetailPostViewController: BaseVC<DetailPostViewModel> {
             $0.height.equalTo(83)
             $0.leading.trailing.equalToSuperview().inset(30)
         }
-
+        
         commentTableView.snp.makeConstraints {
             $0.top.equalTo(enterCommentTextView.snp.bottom).offset(30)
             $0.leading.trailing.equalToSuperview().inset(30)
@@ -172,12 +166,12 @@ extension DetailPostViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CommentCell.identifier, for: indexPath) as? CommentCell else { return UITableViewCell() }
-
+        
         cell.selectionStyle = .none
-
+        
         return cell
     }
 }
