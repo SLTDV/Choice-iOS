ee5f7c4ccddb0e3727db27b0fbbdd4e4587d8031
Mindohyeon <min50875@naver.com>
2023-01-07 22:42:00 +0900

    :sparkles: Add callToProfileData

--- Sources/Present/Main/Profile/ViewModel/ProfileViewModel.swift
+++ Sources/Present/Main/Profile/ViewModel/ProfileViewModel.swift
@@ -1,9 +1,34 @@
 import Foundation
+import Alamofire
+import RxSwift
+
+protocol ProfileDataProtocol: AnyObject {
+    var profileData: PublishSubject<[ProfileModel]> { get set }
+}
 
 class ProfileViewModel: BaseViewModel {
-    
+    weak var delegate: ProfileDataProtocol?
     
     func callToProfileData() {
-        let url = 
+        let url = APIConstants.profileURL
+        let headers: HTTPHeaders = ["Content-Type": "application/json"]
+        
+        AF.request(url,
+                   method: .get,
+                   headers: headers,
+                   interceptor: JwtRequestInterceptor())
+        .responseData { [weak self] response in
+            
+            switch response.result {
+            case .success(let data):
+                let decodeResponse = try? JSONDecoder().decode(ProfileModel.self, from: data)
+                print("decodeResponse = \(decodeResponse)")
+                
+                
+            case .failure(let error):
+                print("error = \(error.localizedDescription)")
+            }
+            
+        }
     }
 }
