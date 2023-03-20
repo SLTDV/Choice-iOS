ab80a09641e8b6ba99c7e41bbe949289e543ec11
Mindohyeon <min50875@naver.com>
2023-03-20 16:00:19 +0900

    :recycle: warning 해결

--- Sources/Present/Common/Cell/PostCell.swift
+++ Sources/Present/Common/Cell/PostCell.swift
@@ -126,13 +126,13 @@ final class PostCell: UITableViewCell, PostTableViewCellButtonDelegate{
         firstPostImageView.layer.borderColor = UIColor.clear.cgColor
         secondPostImageView.layer.borderColor = UIColor.clear.cgColor
         
-        firstPostVoteButton.then {
+        firstPostVoteButton = firstPostVoteButton.then {
             $0.isEnabled = true
             $0.backgroundColor = .clear
             $0.setTitleColor(.gray, for: .normal)
         }
         
-        secondPostVoteButton.then {
+        secondPostVoteButton = secondPostVoteButton.then {
             $0.isEnabled = true
             $0.backgroundColor = .clear
             $0.setTitleColor(.gray, for: .normal)
@@ -143,13 +143,13 @@ final class PostCell: UITableViewCell, PostTableViewCellButtonDelegate{
         firstPostImageView.layer.borderColor = UIColor.black.cgColor
         secondPostImageView.layer.borderColor = UIColor.clear.cgColor
         
-        firstPostVoteButton.then {
+        firstPostVoteButton = firstPostVoteButton.then {
             $0.isEnabled = false
             $0.backgroundColor = .black
             $0.setTitleColor(.white, for: .normal)
         }
         
-        secondPostVoteButton.then {
+        secondPostVoteButton = secondPostVoteButton.then {
             $0.isEnabled = true
             $0.backgroundColor = .clear
             $0.setTitleColor(.gray, for: .normal)
@@ -160,13 +160,13 @@ final class PostCell: UITableViewCell, PostTableViewCellButtonDelegate{
         firstPostImageView.layer.borderColor = UIColor.clear.cgColor
         secondPostImageView.layer.borderColor = UIColor.black.cgColor
         
-        firstPostVoteButton.then {
+        firstPostVoteButton = firstPostVoteButton.then {
             $0.isEnabled = true
             $0.backgroundColor = .clear
             $0.setTitleColor(.gray, for: .normal)
         }
         
-        secondPostVoteButton.then {
+        secondPostVoteButton = secondPostVoteButton.then {
             $0.isEnabled = false
             $0.backgroundColor = .black
             $0.setTitleColor(.white, for: .normal)
