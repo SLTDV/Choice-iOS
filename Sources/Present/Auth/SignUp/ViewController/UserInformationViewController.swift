a24fbaeb59a08d20a429b8387d38f44a8784dcfa
Heesang <80827042+Heeesang@users.noreply.github.com>
2023-03-24 09:10:35 +0900

    :lipstick: UserInformationVC

--- Sources/Present/Auth/SignUp/ViewController/UserInformationViewController.swift
+++ Sources/Present/Auth/SignUp/ViewController/UserInformationViewController.swift
@@ -16,20 +16,54 @@ final class UserInformationViewController: BaseVC<UserInformationViewModel> {
         $0.layer.cornerRadius = 15
     }
     
+    private let imagePickerController = UIImagePickerController()
+    
+    private let userNameLabel = UILabel().then {
+        $0.text = "닉네임"
+        $0.font = .systemFont(ofSize: 14, weight: .semibold)
+    }
+    
+    private let underLineView = UIView().then {
+        $0.backgroundColor = .black
+    }
+    
+    private let completeButton = UIButton().then {
+        $0.setTitle("완료", for: .normal)
+        $0.backgroundColor = .black
+        $0.layer.cornerRadius = 8
+    }
+    
     override func addView() {
-        view.addSubviews(profileImageView, editProfileImageButton)
+        view.addSubviews(profileImageView, editProfileImageButton, userNameLabel, underLineView, completeButton)
     }
     
     override func setLayout() {
         profileImageView.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.top.equalToSuperview().offset(40)
-            $0.size.equalTo(100)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(80)
+            $0.size.equalTo(140)
         }
         
         editProfileImageButton.snp.makeConstraints {
             $0.trailing.equalTo(profileImageView.snp.trailing)
             $0.bottom.equalTo(profileImageView.snp.bottom)
+        }
+        
+        userNameLabel.snp.makeConstraints {
+            $0.top.equalTo(profileImageView.snp.bottom).offset(80)
+            $0.centerX.equalToSuperview()
+        }
+        
+        underLineView.snp.makeConstraints {
+            $0.top.equalTo(userNameLabel.snp.bottom).offset(10)
+            $0.leading.trailing.equalToSuperview().inset(100)
+            $0.height.equalTo(1)
+        }
+        
+        completeButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(64)
+            $0.leading.trailing.equalToSuperview().inset(26)
+            $0.height.equalTo(49)
         }
     }
 }
