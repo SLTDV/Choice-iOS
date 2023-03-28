4892559ba7d4c68e464f74bfedbb9785384aa984
Heesang <80827042+Heeesang@users.noreply.github.com>
2023-03-28 11:00:44 +0900

    :zap: callToSignUpAPI 함수 이동 -> ProfileInfo

--- Sources/Present/Auth/SignUp/UserProfileInfo/ViewModel/UserProfileInfoViewModel.swift
+++ Sources/Present/Auth/SignUp/UserProfileInfo/ViewModel/UserProfileInfoViewModel.swift
@@ -1,5 +1,26 @@
 import UIKit
 
 final class UserProfileInfoViewModel: BaseViewModel {
-    
+    func callToSignUpAPI(nickname: String, email: String, password: String, profileImgUrl: String?){
+        let url = APIConstants.signUpURL
+        let body : Parameters = [
+            "email" : email,
+            "password" : password,
+            "nickname" : nickname,
+            "profileImgUrl" : profileImgUrl ?? ""
+        ]
+        
+        AF.request(url,
+                   method: .post,
+                   parameters: body,
+                   encoding: JSONEncoding.default).responseData { response in
+            switch response.response?.statusCode {
+            case 201:
+                self.coordinator.navigate(to: .popVCIsRequired)
+            default:
+                print(response.response?.statusCode ?? 0)
+                return
+            }
+        }
+    }
 }
--- Sources/Present/Auth/SignUp/UserSecurityInfo/ViewModel/UserSecurityInfoViewModel.swift
+++ Sources/Present/Auth/SignUp/UserSecurityInfo/ViewModel/UserSecurityInfoViewModel.swift
@@ -1,30 +1,7 @@
 import Foundation
 import Alamofire
 
-final class UserSecurityInfoViewModel: BaseViewModel {
-    func callToSignUpAPI(nickname: String, email: String, password: String, profileImgUrl: String?){
-        let url = APIConstants.signUpURL
-        let body : Parameters = [
-            "email" : email,
-            "password" : password,
-            "nickname" : nickname,
-            "profileImgUrl" : profileImgUrl ?? ""
-        ]
-        
-        AF.request(url,
-                   method: .post,
-                   parameters: body,
-                   encoding: JSONEncoding.default).responseData { response in
-            switch response.response?.statusCode {
-            case 201:
-                self.coordinator.navigate(to: .popVCIsRequired)
-            default:
-                print(response.response?.statusCode ?? 0)
-                return 
-            }
-        }
-    }
-    
+final class UserSecurityInfoViewModel: BaseViewModel {    
     func isValidEmail(email: String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
