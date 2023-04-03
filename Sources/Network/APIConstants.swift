de73cf9798d2245914d2211a4b45d64e4948898d
Heesang <80827042+Heeesang@users.noreply.github.com>
2023-04-03 16:04:56 +0900

    :memo: ::

--- Sources/Network/APIConstants.swift
+++ Sources/Network/APIConstants.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 struct APIConstants {
-    static let baseURL = "https://port-0-choice-backend-p8xrq2mlfhw9efx.sel3.cloudtype.app/8080"
+    static let baseURL = "http://10.82.17.76:81"
     
     //SignIn
     static let signInURL = baseURL + "/auth/signin"
