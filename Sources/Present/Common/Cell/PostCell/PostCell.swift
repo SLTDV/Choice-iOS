dd4af146d348f31c771ce58430255779c309c3fb
Mindohyeon <min50875@naver.com>
2023-04-13 22:08:18 +0900

    :bug: conflict 해결

--- Sources/Present/Common/Cell/PostCell/PostCell.swift
+++ Sources/Present/Common/Cell/PostCell/PostCell.swift
@@ -27,7 +27,7 @@ final class PostCell: UITableViewCell {
     static let identifier = "PostCellIdentifier"
     
     private let titleLabel = UILabel().then {
-        $0.font = .systemFont(ofSize: 16, weight: .semibold)
+        $0.font = .systemFont(ofSize: 21, weight: .semibold)
     }
     
     private let contentLabel = UILabel().then {
@@ -50,10 +50,6 @@ final class PostCell: UITableViewCell {
         $0.contentMode = .scaleToFill
     }
     
-    private let firstVoteOptionBackgroundView = VoteOptionView()
-    
-    private let secondVoteOptionBackgroundView = VoteOptionView()
-    
     private let secondPostImageView = UIImageView().then {
         $0.clipsToBounds = true
         $0.layer.cornerRadius = 25
@@ -63,23 +59,17 @@ final class PostCell: UITableViewCell {
     
     private lazy var firstPostVoteButton = UIButton().then {
         $0.tag = 0
-        $0.setTitle("✓", for: .normal)
-        $0.setTitleColor(.gray, for: .normal)
-        $0.layer.borderWidth = 1
+        $0.setTitleColor(.white, for: .normal)
         $0.layer.cornerRadius = 10
-        $0.layer.borderColor = ChoiceAsset.Colors.grayDark.color.cgColor
-        $0.backgroundColor = .clear
+        $0.backgroundColor = ChoiceAsset.Colors.grayBackground.color
         $0.addTarget(self, action: #selector(PostVoteButtonDidTap(_:)), for: .touchUpInside)
     }
     
     private lazy var secondPostVoteButton = UIButton().then {
         $0.tag = 1
-        $0.setTitle("✓", for: .normal)
-        $0.setTitleColor(.gray, for: .normal)
-        $0.layer.borderWidth = 1
+        $0.setTitleColor(.white, for: .normal)
         $0.layer.cornerRadius = 10
-        $0.layer.borderColor = ChoiceAsset.Colors.grayDark.color.cgColor
-        $0.backgroundColor = .clear
+        $0.backgroundColor = ChoiceAsset.Colors.grayBackground.color
         $0.addTarget(self, action: #selector(PostVoteButtonDidTap(_:)), for: .touchUpInside)
     }
     
@@ -141,21 +131,19 @@ final class PostCell: UITableViewCell {
         contentView.addSubviews(titleLabel, contentLabel, removePostButton, firstPostImageView,
                                 secondPostImageView, firstPostVoteButton, secondPostVoteButton,
                                 participantsCountLabel, commentCountLabel)
-        firstPostImageView.addSubview(firstVoteOptionBackgroundView)
-        secondPostImageView.addSubview(secondVoteOptionBackgroundView)
     }
     
     private func setLayout() {
         titleLabel.snp.makeConstraints {
             $0.top.equalToSuperview().inset(31)
-            $0.leading.equalToSuperview().inset(23)
+            $0.leading.equalToSuperview().inset(31)
             $0.trailing.equalToSuperview()
             $0.height.equalTo(21)
         }
         
         contentLabel.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom).offset(16)
-            $0.leading.trailing.equalToSuperview().inset(23)
+            $0.leading.trailing.equalToSuperview().inset(31)
         }
         
         removePostButton.snp.makeConstraints {
@@ -165,81 +153,67 @@ final class PostCell: UITableViewCell {
         
         firstPostImageView.snp.makeConstraints {
             $0.top.equalTo(contentLabel.snp.bottom).offset(24)
-            $0.leading.equalToSuperview().inset(21)
+            $0.leading.equalToSuperview().inset(31)
             $0.width.equalTo(134)
             $0.height.equalTo(145)
         }
         
         secondPostImageView.snp.makeConstraints {
             $0.top.equalTo(contentLabel.snp.bottom).offset(24)
-            $0.trailing.equalToSuperview().inset(21)
+            $0.trailing.equalToSuperview().inset(31)
             $0.width.equalTo(134)
             $0.height.equalTo(145)
         }
         
-        firstVoteOptionBackgroundView.snp.makeConstraints {
-            $0.height.equalTo(52)
-            $0.leading.trailing.bottom.equalToSuperview()
-        }
-        
-        secondVoteOptionBackgroundView.snp.makeConstraints {
-            $0.height.equalTo(52)
-            $0.leading.trailing.bottom.equalToSuperview()
-        }
-        
         firstPostVoteButton.snp.makeConstraints {
-            $0.top.equalTo(firstPostImageView.snp.bottom).offset(16)
-            $0.leading.equalToSuperview().inset(38)
-            $0.width.equalTo(101)
-            $0.height.equalTo(38)
+            $0.top.equalTo(firstPostImageView.snp.bottom).offset(26)
+            $0.leading.equalTo(firstPostImageView.snp.leading)
+            $0.trailing.equalTo(firstPostImageView.snp.trailing)
+            $0.height.equalTo(56)
         }
         
         secondPostVoteButton.snp.makeConstraints {
-            $0.top.equalTo(secondPostImageView.snp.bottom).offset(16)
-            $0.trailing.equalToSuperview().inset(38)
-            $0.width.equalTo(101)
-            $0.height.equalTo(38)
+            $0.top.equalTo(secondPostImageView.snp.bottom).offset(26)
+            $0.leading.equalTo(secondPostImageView.snp.leading)
+            $0.trailing.equalTo(secondPostImageView.snp.trailing)
+            $0.height.equalTo(56)
         }
         
         participantsCountLabel.snp.makeConstraints {
-            $0.top.equalTo(firstPostVoteButton.snp.bottom)
+            $0.top.equalTo(firstPostVoteButton.snp.bottom).offset(20)
             $0.leading.equalToSuperview().inset(33)
             $0.bottom.equalToSuperview().inset(16)
         }
         
         commentCountLabel.snp.makeConstraints {
-            $0.top.equalTo(firstPostVoteButton.snp.bottom)
+            $0.top.equalTo(firstPostVoteButton.snp.bottom).offset(20)
             $0.leading.equalTo(participantsCountLabel.snp.trailing).offset(13)
             $0.bottom.equalToSuperview().inset(16)
         }
     }
     
     private func setHomeVotePostLayout(voting: Int) {
+        firstPostVoteButton.setTitleColor(.white, for: .normal)
+        secondPostVoteButton.setTitleColor(.white, for: .normal)
         switch voting {
         case 1:
             firstPostVoteButton.isEnabled = false
             firstPostVoteButton.backgroundColor = .black
-            firstPostVoteButton.setTitleColor(.white, for: .normal)
             
             secondPostVoteButton.isEnabled = true
-            secondPostVoteButton.backgroundColor = .clear
-            secondPostVoteButton.setTitleColor(.gray, for: .normal)
+            secondPostVoteButton.backgroundColor = ChoiceAsset.Colors.grayVoteButton.color
         case 2:
             firstPostVoteButton.isEnabled = true
-            firstPostVoteButton.backgroundColor = .clear
-            firstPostVoteButton.setTitleColor(.gray, for: .normal)
+            firstPostVoteButton.backgroundColor = ChoiceAsset.Colors.grayVoteButton.color
             
             secondPostVoteButton.isEnabled = false
             secondPostVoteButton.backgroundColor = .black
-            secondPostVoteButton.setTitleColor(.white, for: .normal)
         default:
             firstPostVoteButton.isEnabled = true
-            firstPostVoteButton.setTitleColor(.gray, for: .normal)
-            firstPostVoteButton.backgroundColor = .clear
+            firstPostVoteButton.backgroundColor = ChoiceAsset.Colors.grayVoteButton.color
             
             secondPostVoteButton.isEnabled = true
-            secondPostVoteButton.setTitleColor(.gray, for: .normal)
-            secondPostVoteButton.backgroundColor = .clear
+            secondPostVoteButton.backgroundColor = ChoiceAsset.Colors.grayVoteButton.color
         }
     }
     
@@ -261,14 +235,10 @@ final class PostCell: UITableViewCell {
     
     private func votePostButtonLayout(voting: Int) {
         firstPostVoteButton.snp.updateConstraints {
-            $0.leading.equalToSuperview().inset(20)
-            $0.width.equalTo(144)
             $0.height.equalTo(52)
         }
         
         secondPostVoteButton.snp.updateConstraints {
-            $0.trailing.equalToSuperview().inset(20)
-            $0.width.equalTo(144)
             $0.height.equalTo(52)
         }
         
@@ -296,10 +266,10 @@ final class PostCell: UITableViewCell {
         guard let secondImageUrl = URL(string: model.secondImageUrl) else { return }
         titleLabel.text = model.title
         contentLabel.text = model.content
-        firstVoteOptionBackgroundView.setVoteOptionLabel(model.firstVotingOption)
-        secondVoteOptionBackgroundView.setVoteOptionLabel(model.secondVotingOption)
         firstPostImageView.kf.setImage(with: firstImageUrl)
         secondPostImageView.kf.setImage(with: secondImageUrl)
+        firstPostVoteButton.setTitle(model.firstVotingOption, for: .normal)
+        secondPostVoteButton.setTitle(model.secondVotingOption, for: .normal)
         switch type {
         case .home:
             setHomeVotePostLayout(voting: model.votingState)
--- Sources/Present/Main/Home/ViewController/HomeViewController.swift
+++ Sources/Present/Main/Home/ViewController/HomeViewController.swift
@@ -56,6 +56,7 @@ final class HomeViewController: BaseVC<HomeViewModel>, PostItemsProtocol, PostVo
                                                       cellType: PostCell.self)) { (row, data, cell) in
             cell.changeCellData(with: data, type: .home)
             cell.postVoteButtonDelegate = self
+            cell.separatorInset = UIEdgeInsets.zero
         }.disposed(by: disposeBag)
         
         postTableView.rx.modelSelected(PostModel.self)
