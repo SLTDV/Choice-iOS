6e40ac33ae79c46282cc5c55460e7bac589bbe7b
Heesang <80827042+Heeesang@users.noreply.github.com>
2023-04-17 12:20:36 +0900

    :zap: 조건 수정

--- Sources/Present/Auth/SignIn/ViewController/SignInViewController.swift
+++ Sources/Present/Auth/SignIn/ViewController/SignInViewController.swift
@@ -63,7 +63,7 @@ final class SignInViewController: BaseVC<SignInViewModel> {
                 LoadingIndicator.showLoading(text: "")
                 DispatchQueue.main.async {
                     self?.viewModel.callToSignInAPI(email: email, password: password){ [weak self] isComplete in
-                        if !isComplete {
+                        if isComplete == false{
                             self?.showWarningLabel(warning: "아이디 또는 비밀번호가 잘못되었습니다.")
                             DispatchQueue.main.async {
                                 self?.inputIdTextField.shake()
