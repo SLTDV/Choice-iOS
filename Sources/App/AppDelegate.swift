2fae634f8a4af47d304947f41719757cc701a545
Mindohyeon <min50875@naver.com>
2023-04-17 10:28:14 +0900

    :recycle: delay 삭제

--- Sources/App/AppDelegate.swift
+++ Sources/App/AppDelegate.swift
@@ -5,7 +5,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        sleep(2)
         return true
     }
 
