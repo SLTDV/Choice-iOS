677ed99c163c66a8db6b05892ff7d1183f89092a
Mindohyeon <min50875@naver.com>
2023-04-27 17:46:34 +0900

    :recycle: 코드정리

--- Sources/Util/Extension/String/StringToDate.swift
+++ Sources/Util/Extension/String/StringToDate.swift
@@ -3,11 +3,10 @@ import Foundation
 extension String {
     func getStringToDate() -> Date {
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS"
-        formatter.locale = Locale(identifier: "Ko_Kr")
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSS"
+        formatter.locale = Locale(identifier: "kr")
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
         
-        print("self = \(Date())")
-        print("return \(formatter.date(from: self))")
         return formatter.date(from: self)!
     }
 }
