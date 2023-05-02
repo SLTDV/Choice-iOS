d2df2b6e5248fd20aa83dbb4ae6a97b6afe46f4c
Heesang <80827042+Heeesang@users.noreply.github.com>
2023-05-02 15:07:25 +0900

    :lipstick: againRequest

--- Sources/Present/Auth/SignUp/RegistrationPhoneNum/ViewController/RegistrationPhoneNumberViewController.swift
+++ Sources/Present/Auth/SignUp/RegistrationPhoneNum/ViewController/RegistrationPhoneNumberViewController.swift
@@ -5,6 +5,8 @@ import RxCocoa
 final class RegistrationPhoneNumberViewController: BaseVC<RegistrationPhoneNumberViewModel> {
     private let disposeBag = DisposeBag()
     
+    private var isValidPhoneNumber = true
+    
     private lazy var restoreFrameYValue = 0.0
     
     private let emailLabel = UILabel().then {
@@ -30,13 +32,29 @@ final class RegistrationPhoneNumberViewController: BaseVC<RegistrationPhoneNumbe
     }
     
     private let CertificationNumberTextfield = UITextField().then {
-        $0.isHidden = true
         $0.addLeftPadding()
         $0.placeholder = "인증번호 입력"
         $0.layer.borderColor = ChoiceAsset.Colors.grayMedium.color.cgColor
         $0.layer.borderWidth = 1
         $0.layer.cornerRadius = 8
         $0.keyboardType = .numberPad
+    }
+    
+    private let countLabel = UILabel().then {
+        $0.textAlignment = .center
+        $0.textColor = .black
+    }
+    
+    private let againRequestLabel = UILabel().then {
+        $0.text = "인증번호가 오지 않나요?"
+        $0.font = .systemFont(ofSize: 12, weight: .medium)
+        $0.textColor = .gray
+    }
+    
+    private let againRequestButton = UIButton().then {
+        $0.setTitle("재요청", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 13, weight: .semibold)
+        $0.setTitleColor(.black, for: .normal)
     }
     
     private lazy var signUpButton = UIButton().then {
@@ -100,14 +118,36 @@ final class RegistrationPhoneNumberViewController: BaseVC<RegistrationPhoneNumbe
         
         CertificationRequestButton.rx.tap
             .withLatestFrom(phoneNumberObservable)
-            .take(3)
             .bind(with: self) { owner, inputPhoneNumber in
-                owner.viewModel.requestCertification(inputPhoneNumber: inputPhoneNumber) { inVaild in
-                    if inVaild {
-                        owner.CertificationNumberTextfield.isHidden = false
-                    } else {
-                        self.showWarningLabel(warning: "*이미 인증된 전화번호입니다")
+                if owner.isValidPhoneNumber {
+                    owner.viewModel.requestCertification(inputPhoneNumber: inputPhoneNumber) { inVaild in
+                        if inVaild {
+                            owner.CertificationRequestButton.backgroundColor = ChoiceAsset.Colors.grayVoteButton.color
+                            owner.setupPossibleBackgroundTimer()
+                            owner.CertificationNumberTextfield.isHidden = false
+                        } else {
+                            self.showWarningLabel(warning: "*이미 인증된 전화번호입니다")
+                        }
                     }
+                }
+            }.disposed(by: disposeBag)
+    }
+    
+    private func setupPossibleBackgroundTimer() {
+        let count = 180
+        
+        isValidPhoneNumber = false
+        
+        Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
+            .take(count)
+            .map { count - $0 }
+            .bind(with: self) { owner, remainingSeconds in
+                let minutes = remainingSeconds / 60
+                let seconds = remainingSeconds % 60
+                owner.countLabel.text = String(format: "%02d:%02d", minutes, seconds)
+                
+                if remainingSeconds == 0 {
+                    owner.isValidPhoneNumber = true
                 }
             }.disposed(by: disposeBag)
     }
@@ -131,7 +171,10 @@ final class RegistrationPhoneNumberViewController: BaseVC<RegistrationPhoneNumbe
     
     override func addView() {
         view.addSubviews(emailLabel,inputPhoneNumberTextfield, CertificationRequestButton,
-                         CertificationNumberTextfield, warningLabel, signUpButton)
+                         CertificationNumberTextfield, warningLabel, signUpButton,
+                         againRequestLabel, againRequestButton)
+        
+        CertificationNumberTextfield.addSubview(countLabel)
     }
     
     override func setLayout() {
@@ -158,6 +201,21 @@ final class RegistrationPhoneNumberViewController: BaseVC<RegistrationPhoneNumbe
             $0.top.equalTo(inputPhoneNumberTextfield.snp.bottom).offset(20)
             $0.leading.trailing.equalToSuperview().inset(26)
             $0.height.equalTo(51)
+        }
+        
+        countLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(25)
+        }
+        
+        againRequestLabel.snp.makeConstraints {
+            $0.centerY.equalTo(againRequestButton)
+            $0.centerX.equalToSuperview().offset(-20)
+        }
+        
+        againRequestButton.snp.makeConstraints {
+            $0.top.equalTo(CertificationNumberTextfield.snp.bottom).offset(15)
+            $0.leading.equalTo(againRequestLabel.snp.trailing).offset(8)
         }
         
         warningLabel.snp.makeConstraints {
