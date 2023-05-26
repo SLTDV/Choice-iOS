575f64b3e1a054e80f3f3c3f69f7e293b8aca671
Mindohyeon <min50875@naver.com>
2023-05-26 10:58:53 +0900

    Update 소수점 제거

--- Projects/Shared/Sources/Util/Class/CalculateToVoteCountPercentage.swift
+++ Projects/Shared/Sources/Util/Class/CalculateToVoteCountPercentage.swift
@@ -2,15 +2,16 @@ import Foundation
 
 public enum CalculateToVoteCountPercentage {
     public static func calculateToVoteCountPercentage(firstVotingCount: Double, secondVotingCount: Double) -> (String, String, Int, Int) {
-        let sum = firstVotingCount + secondVotingCount
-        var firstP = firstVotingCount / sum * 100.0
-        var secondP = secondVotingCount / sum * 100.0
+        let sum = Double(firstVotingCount) + Double(secondVotingCount)
+        print("fasdf = \(firstVotingCount / sum)")
+        let firstP = firstVotingCount / sum * 100.0
+        let secondP = secondVotingCount / sum * 100.0
         
-        firstP = firstP.isNaN ? 0.0 : firstP
-        secondP = secondP.isNaN ? 0.0 : secondP
+//        firstP = firstP.isNaN ? 0 : firstP
+//        secondP = secondP.isNaN ? 0 : secondP
         
-        let firstStr = String(format: "%0.2f", firstP)
-        let secondStr = String(format: "%0.2f", secondP)
+        let firstStr = String(firstP)
+        let secondStr = String(secondP)
         
         return (firstStr, secondStr, Int(firstVotingCount), Int(secondVotingCount))
     }
