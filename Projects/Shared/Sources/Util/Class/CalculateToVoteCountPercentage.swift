b483c2d55894d0c967d30010a42c5ef96a3f93b7
Mindohyeon <min50875@naver.com>
2023-05-31 12:15:42 +0900

    Refactor CalculateToVoteCountPercentage

--- Projects/Shared/Sources/Util/Class/CalculateToVoteCountPercentage.swift
+++ Projects/Shared/Sources/Util/Class/CalculateToVoteCountPercentage.swift
@@ -1,7 +1,10 @@
 import Foundation
 
 public enum CalculateToVoteCountPercentage {
-    public static func calculateToVoteCountPercentage(firstVotingCount: Double, secondVotingCount: Double) -> (String, String, Int, Int) {
+    public static func calculateToVoteCountPercentage(
+        firstVotingCount: Double,
+        secondVotingCount: Double
+    ) -> (String, String, Int, Int) {
         let sum = firstVotingCount + secondVotingCount
         var firstP = firstVotingCount / sum * 100
         var secondP = secondVotingCount / sum * 100
@@ -9,9 +12,11 @@ public enum CalculateToVoteCountPercentage {
         firstP = firstP.isNaN ? 0 : firstP
         secondP = secondP.isNaN ? 0 : secondP
         
-//        let firstStr = (sum != 0) ? String(Int(firstP)) : String(firstP)
-//        let secondStr = (sum != 0) ? String(Int(secondP)) : String(secondP)
-        
-        return (String(Int(firstP)), String(Int(secondP)), Int(firstVotingCount), Int(secondVotingCount))
+        return (
+            String(Int(firstP)),
+            String(Int(secondP)),
+            Int(firstVotingCount),
+            Int(secondVotingCount)
+        )
     }
 }
