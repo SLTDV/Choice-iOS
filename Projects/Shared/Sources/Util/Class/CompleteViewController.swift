04c02e86ce0cfd5716daffd83f9fa6a1daa558e3
Mindohyeon <min50875@naver.com>
2023-06-15 17:44:24 +0900

    Update CompleteViewController

--- Projects/Shared/Sources/Util/Class/CompleteViewController.swift
+++ Projects/Shared/Sources/Util/Class/CompleteViewController.swift
@@ -1,7 +1,22 @@
 import UIKit
 import SnapKit
-import Theng
+import Lottie
 
-class CompleteViewController: UIViewController {
-    private let animationView: ANimation
+public class CompleteViewController: UIViewController {
+    private var text: String
+    
+    private let animationView: LottieAnimationView = {
+       let lottieView = LottieAnimationView(name: "lottieFile")
+       lottieView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+       return lottieView
+    }()
+    
+    public init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?, text: String) {
+        self.text = text
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
