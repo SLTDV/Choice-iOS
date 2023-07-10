79293fe81ccd2b28cdc1048e7245ef823cb8e73e
Mindohyeon <min50875@naver.com>
2023-07-10 13:58:24 +0900

    Add DownSampling

---
+++ Projects/Shared/Sources/Util/Class/Downsampling.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class Downsampling {
+    func optimization(imageAt imageURL: URL, to pointSize: CGSize, scale: CGFloat) -> UIImage {
+        let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
+        let imageSource = CGImageSourceCreateWithURL(imageURL as CFURL, imageSourceOptions)!
+        
+        let maxDimensionInPixels = max(pointSize.width, pointSize.height) * scale
+        let downsampleOptions = [
+            kCGImageSourceCreateThumbnailFromImageAlways: true,
+            kCGImageSourceShouldCacheImmediately: true,
+            kCGImageSourceCreateThumbnailWithTransform: true,
+            kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels
+        ] as [CFString : Any] as CFDictionary
+        
+        let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions)!
+        return UIImage(cgImage: downsampledImage)
+    }
+}
