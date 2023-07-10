d50ef32a58bb77929bf13c67dcfc4a755161b6dd
Mindohyeon <min50875@naver.com>
2023-07-10 23:32:15 +0900

    Error 이미지 버벅거림

--- Projects/Shared/Sources/Util/Class/Downsampling.swift
+++ Projects/Shared/Sources/Util/Class/Downsampling.swift
@@ -7,37 +7,29 @@ public enum Downsampling {
         scale: CGFloat,
         completion: @escaping (UIImage?) -> Void
     ) {
-        let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
-        var returnImage: UIImage?
-        URLSession.shared.dataTask(with: imageURL) { data, response, error in
-            if let error = error {
-                completion(nil)
-                print("Tast Error = \(error)")
-                return
-            }
+        DispatchQueue.global().async {
+            let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
+            let imageSource = CGImageSourceCreateWithURL(imageURL as CFURL, imageSourceOptions)!
             
-            guard let data = data, let imageSource = CGImageSourceCreateWithData(data as CFData, imageSourceOptions) else {
-                completion(nil)
-                return
-            }
+            let maxDimensionInPixels = max(pointSize.width, pointSize.height) * scale
+            let downsampleOptions = [
+                kCGImageSourceCreateThumbnailFromImageAlways: true,
+                kCGImageSourceShouldCacheImmediately: true,
+                kCGImageSourceCreateThumbnailWithTransform: true,
+                kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels
+            ] as [CFString : Any] as CFDictionary
             
-            DispatchQueue.main.async {
-                let maxDimensionInPixels = max(pointSize.width, pointSize.height) * scale
-                let downsampleOptions = [
-                    kCGImageSourceCreateThumbnailFromImageAlways: true,
-                    kCGImageSourceShouldCacheImmediately: true,
-                    kCGImageSourceCreateThumbnailWithTransform: true,
-                    kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels
-                ] as [CFString : Any] as CFDictionary
-                
-                guard let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions) else {
+            guard let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions) else {
+                DispatchQueue.main.async {
                     completion(nil)
-                    return
                 }
-                
-                returnImage = UIImage(cgImage: downsampledImage)
+                return
+            }
+            DispatchQueue.main.async {
+                let returnImage = UIImage(cgImage: downsampledImage)
+                print("return = \(returnImage)")
                 completion(returnImage)
             }
-        }.resume()
+        }
     }
 }
