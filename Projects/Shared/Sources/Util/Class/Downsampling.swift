43a60e3c3f21f5ce023cead232b0c2cea25d5a35
Mindohyeon <min50875@naver.com>
2023-07-13 20:41:07 +0900

    코드 정리

--- Projects/Shared/Sources/Util/Class/Downsampling.swift
+++ Projects/Shared/Sources/Util/Class/Downsampling.swift
@@ -10,47 +10,69 @@ public enum Downsampling {
         completion: @escaping (UIImage?) -> Void
     ) {
         // 이미지 캐시 확인
-        if let cachedImage = imageCache.object(forKey: imageURL as NSURL) {
-            DispatchQueue.main.async {
-                completion(cachedImage)
+        DispatchQueue.global().async {
+            
+            if let cachedImage = imageCache.object(
+                forKey: imageURL as NSURL
+            ) {
+                DispatchQueue.main.async {
+                    completion(cachedImage)
+                }
+                return
             }
-            return
+            
+            let maxDimensionInPixels = max(
+                pointSize.width,
+                pointSize.height
+            ) * scale
+            let downsampleOptions = [
+                kCGImageSourceCreateThumbnailFromImageAlways: true,
+                kCGImageSourceShouldCacheImmediately: false,
+                kCGImageSourceCreateThumbnailWithTransform: true,
+                kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels,
+            ] as [CFString : Any] as CFDictionary
+            
+            URLSession.shared.dataTask(with: imageURL) { data, response, error in
+                if let error = error {
+                    print("Error! - \(error)")
+                }
+                guard let data = data,
+                      let imageSource = CGImageSourceCreateWithData(
+                        data as CFData, nil
+                      ) else {
+                    DispatchQueue.main.async {
+                        completion(nil)
+                    }
+                    return
+                }
+                
+                guard let downsampledImage = CGImageSourceCreateThumbnailAtIndex(
+                    imageSource,
+                    0,
+                    downsampleOptions as CFDictionary
+                ) else {
+                    DispatchQueue.main.async {
+                        completion(nil)
+                    }
+                    return
+                }
+                
+                let downsampledUIImage = UIImage(
+                    data: UIImage(
+                        cgImage: downsampledImage
+                    ).jpegData(compressionQuality: 0.7)!
+                )
+                
+                imageCache.setObject(
+                    downsampledUIImage!,
+                    forKey: imageURL as NSURL
+                )
+                
+                DispatchQueue.main.async {
+                    completion(downsampledUIImage)
+                }
+            }.resume()
         }
-        
-        let maxDimensionInPixels = max(pointSize.width, pointSize.height) * scale
-        let downsampleOptions = [
-            kCGImageSourceCreateThumbnailFromImageAlways: true,
-            kCGImageSourceShouldCacheImmediately: false,
-            kCGImageSourceCreateThumbnailWithTransform: true,
-            kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels,
-        ] as [CFString : Any] as CFDictionary
-        
-        URLSession.shared.dataTask(with: imageURL) { data, response, error in
-            if let error = error {
-                print("Error! - \(error)")
-            }
-            guard let data = data, let imageSource = CGImageSourceCreateWithData(data as CFData, nil) else {
-                DispatchQueue.main.async {
-                    completion(nil)
-                }
-                return
-            }
-            
-            guard let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions as CFDictionary) else {
-                DispatchQueue.main.async {
-                    completion(nil)
-                }
-                return
-            }
-            
-            let downsampledUIImage = UIImage(data: UIImage(cgImage: downsampledImage).jpegData(compressionQuality: 0.8)!)
-            
-            imageCache.setObject(downsampledUIImage!, forKey: imageURL as NSURL)
-            
-            DispatchQueue.main.async {
-                completion(downsampledUIImage)
-            }
-        }.resume()
     }
     
     public static func generateUniqueImageURL(imageURL baseURL: String, postID: Int, imageIndex: Int) -> URL? {
