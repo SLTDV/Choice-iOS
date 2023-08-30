f4d696a570214aead5ce1e59750ba90a30bb08c3
Heesang <80827042+Heeesang@users.noreply.github.com>
2023-08-30 17:46:13 +0900

    Add DetailMenuModalVC 추가

---
+++ Projects/App/Sources/Present/Detail/ViewController/Modal/DetailMenuModalViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
+import Shared
+
+final class DetailMenuModalViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+    }
+}
