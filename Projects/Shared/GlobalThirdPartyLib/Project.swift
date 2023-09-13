59e25d111d1ae94b2509b5078833da789b281577
Mindohyeon <min50875@naver.com>
2023-09-13 13:46:28 +0900

    Add Shared - GlobalThirdPartyLib

--- Projects/Shared/GlobalThirdPartyLib/Project.swift
+++ Projects/Shared/GlobalThirdPartyLib/Project.swift
@@ -2,7 +2,7 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 let project = Project.makeModule(
-    name: "Shared",
+    name: "GlobalThirdPartyLib",
     product: .framework,
     packages: [],
     dependencies: [
