306bb13ddc4e865b327e9b4229371acfc47b947c
Mindohyeon <min50875@naver.com>
2023-11-08 18:10:30 +0900

    Add PostList init

--- Projects/App/Sources/Present/Common/Model/PostModel.swift
+++ Projects/App/Sources/Present/Common/Model/PostModel.swift
@@ -17,6 +17,22 @@ class PostList: Codable {
     var votingState: Int
     var participants: Int
     var commentCount: Int
+    
+    init(idx: Int,
+         firstImageUrl: String, secondImageUrl: String, title: String, content: String, firstVotingOption: String, secondVotingOption: String, firstVotingCount: Int, secondVotingCount: Int, votingState: Int, participants: Int, commentCount: Int) {
+        self.idx = idx
+        self.firstImageUrl = firstImageUrl
+        self.secondImageUrl = secondImageUrl
+        self.title = title
+        self.content = content
+        self.firstVotingOption = firstVotingOption
+        self.secondVotingOption = secondVotingOption
+        self.firstVotingCount = firstVotingCount
+        self.secondVotingCount = secondVotingCount
+        self.votingState = votingState
+        self.participants = participants
+        self.commentCount = commentCount
+    }
 }
 
 struct RequestPostModel {
