18355d770df02ea907cfae2ac43b0d48e8a37660
Mindohyeon <min50875@naver.com>
2023-11-17 11:54:44 +0900

    closeApp 함수 NetworkMonitor 모듈로 이동

---
+++ Projects/Core/NetworksMonitor/Sources/AppCloseHandler.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+public struct AppCloseHandler {
+    public static func closedApp() {
+        UIApplication.shared.perform(#selector(NSXPCConnection.suspend))
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            exit(0)
+        }
+    }
+}
